2c20c65fceec9f8e02508e109fa4ef0922c6e25f
Sean Lum <seanjklum@gmail.com>
2016-12-31 03:40:48 -0800

    [v0.0.3-vanilla][FIX] Implemented windows, their CSS rules, and some documentation

--- web-window-vanilla.js
+++ web-window-vanilla.js
@@ -8,7 +8,28 @@ WebWindow.desktopID = 'foreground';
 WebWindow.beforeElementID = 'before-node';
 WebWindow.classes = {
     fadeIn : 'web-window-fade-in',
-    fadeOut : 'web-window-fade-out'
+    fadeOut : 'web-window-fade-out',
+    invisible : 'web-window-invisible',
+    minimize : 'web-window-minimize',
+    maximize : 'web-window-maximize',
+    minimizeAnimation : 'web-window-minimize-animation',
+    maximizeAnimation : 'web-window-maximize-animation',
+    taskFrameWindow : 'task-frame-window-border',
+    taskFrameHeader : 'task-frame-header',
+    taskFrameButtons : 'task-frame-buttons',
+    taskFrameContent : 'task-frame-content',
+    taskFrameDragbar : 'task-frame-dragbar',
+    taskFrameResize : 'task-frame-resize',
+    taskFrameWindowTitle : 'task-frame-window-title',
+    resizer : 'web-window-resizer',
+    resizeTop : 'web-window-resize-top',
+    resizeBottom : 'web-window-resize-bottom',
+    resizeRight : 'web-window-resize-right',
+    resizeLeft : 'web-window-resize-left',
+    resizeTopRight : 'web-window-resize-top-right',
+    resizeTopLeft : 'web-window-resize-top-left',
+    resizeBottomRight : 'web-window-resize-bottom-right',
+    resizeBottomLeft : 'web-window-resize-bottom-left'
 };
 WebWindow.Util = {
     CSS : {
@@ -32,11 +53,23 @@ WebWindow.Util = {
         },
         toggleClass : function(element, className) {
             var found = false;
+            /* - if the class does not exist, supply an empty string.
+               - If the class does exist, then supply the class name.
+               - Break up the scentence into words, begin filtering.
+               - If the class name is found, set found to true, and
+                 remove it from the next step. Remove undefined items.
+               - Place all the words back into the scentence. then
+                 remove empty space. If the class was found, put it
+                 back at the end of the scentence, otherwise, return
+                 the cleaned up class list.
+                */
             var classes = (element.className ? element.className : '')
                 .split('\ ')
                 .filter(function(tempClassName, tempClassIndex, classesOrigin) {
                     if (tempClassName === className) {
                         found = true;
+                        return false;
+                    } else if (tempClassName === 'undefined') {
                         return false;
                     } else {
                         return true;
@@ -99,53 +132,306 @@ WebWindow.Util = {
 /* DOM Elements for Task object */
 WebWindow.TaskFrame = function(data, taskAssistant) {
     var self = this;
-    function create() {
 
-    }
-    function dispose() {
+    var taskFrameWindow = document.createElement('div');
+        taskFrameWindow.className = WebWindow.classes.taskFrameWindow;
+        taskFrameWindow.id = Date.now();
+        taskFrameWindow.addEventListener('click', function(event) {
+            document.getElementById(WebWindow.desktopID)
+                .insertBefore(document.getElementById(taskFrameWindow.id),
+                              document.getElementById(WebWindow.beforeElementID));
+        });
+    var taskFrameWindowTitle = document.createElement('div');
+        taskFrameWindowTitle.className = WebWindow.classes.taskFrameWindowTitle + ' noselect';
+    var taskFrameHeader = document.createElement('div');
+        taskFrameHeader.className = WebWindow.classes.taskFrameHeader + ' noselect';
+    var taskFrameButtons = document.createElement('ul');
+        taskFrameButtons.className = WebWindow.classes.taskFrameButtons + ' noselect';
+    var taskFrameContent = document.createElement('div');
+        taskFrameContent.className = WebWindow.classes.taskFrameContent + ' noselect';
+    var taskFrameDragbar = document.createElement('div');
+        taskFrameDragbar.className = WebWindow.classes.taskFrameDragbar + ' noselect';
+    var taskFrameResize = document.createElement('div');
+        taskFrameResize.className = WebWindow.classes.taskFrameResize + ' noselect';
 
-    }
-    function setDragBar() {
-    
-    }
+
+        /* Resizing control DOM elements, all sides and corners */
+    var resizeTop = document.createElement('div');
+        resizeTop.className = WebWindow.classes.resizeTop + ' ' + WebWindow.classes.resizer;
+    var resizeLeft = document.createElement('div');
+        resizeLeft.className = WebWindow.classes.resizeLeft + ' ' + WebWindow.classes.resizer;
+    var resizeRight = document.createElement('div');
+        resizeRight.className = WebWindow.classes.resizeRight + ' ' + WebWindow.classes.resizer;
+    var resizeBottom = document.createElement('div');
+        resizeBottom.className = WebWindow.classes.resizeBottom + ' ' + WebWindow.classes.resizer;
+    var resizeTopLeft = document.createElement('div');
+        resizeTopLeft.className = WebWindow.classes.resizeTopLeft + ' ' + WebWindow.classes.resizer;
+    var resizeTopRight = document.createElement('div');
+        resizeTopRight.className = WebWindow.classes.resizeTopRight + ' ' + WebWindow.classes.resizer;
+    var resizeBottomLeft = document.createElement('div');
+        resizeBottomLeft.className = WebWindow.classes.resizeBottomLeft + ' ' + WebWindow.classes.resizer;
+    var resizeBottomRight = document.createElement('div');
+        resizeBottomRight.className = WebWindow.classes.resizeBottomRight + ' ' + WebWindow.classes.resizer;
+
+
+    /* Sets up the resize controls with onclick handlers, and has utility functions.
+       Utilities:
+            - firstNumber : extracts the first occuring continuing section of numbers
+                            within a string.
+            - ifSizes : if the task frame does not have style (left, top), and
+                        size (width, height), then it uses the properties of
+                        doucment.getElement, to assign the respective values.
+            - getWindow : returns the window with the id it was created with.
+            - mouseMove : called normally when the mousedown event occurs, it
+                          returns a function which when called will call ifSizes,
+                          and then assigns the callback supplied, which is
+                          triggered when the mouse moves within the window, and
+                          the mouse is still down. When the mouseup event fires,
+                          the onmousemove event is then removed from the window.
+     */
     function setResizeButtons() {
-        function resizeTop(event) {}
-        function resizeLeft(event) {}
-        function resizeRight(event) {}
-        function resizeBottom(event) {}
-        function resizeTopLeft(event) {}
-        function resizeTopRight(event) {}
-        function resizeBottomLeft(event) {}
-        function resizeBottomRight(event) {}
-    }
-    function setTaskFrame() {
 
+        function firstNumber(inputStr) {
+            return Number(/^(\d+)/.exec(inputStr)[0]);
+        }
+
+        function ifSizes(elem) {
+            if (!(elem.style.left)) {
+                elem.style.left = elem.clientLeft + 'px';
+            }
+            if (!(elem.style.top)) {
+                elem.style.top = elem.clientTop + 'px';
+            }
+            if (!(elem.style.width)) {
+                elem.style.width = elem.clientWidth + 'px';
+            }
+            if (!(elem.style.height)) {
+                elem.style.height = elem.clientHeight + 'px';
+            }
+        }
+
+        function getWindow(windowID) {
+            return document.getElementById(taskFrameWindow.id);
+        }
+
+        function mouseMove(callback) {
+            return function() {
+                ifSizes(getWindow());
+                window.onmousemove = callback;
+                window.onmouseup = function() { window.onmousemove = null; }
+            }
+        }
+
+        resizeTop.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.top = (firstNumber(elem.style.top) + event.movementY) + 'px';
+            elem.style.height = (firstNumber(elem.style.height) - event.movementY) + 'px';
+        });
+
+        resizeLeft.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.left = (firstNumber(elem.style.left) + event.movementX) + 'px';
+            elem.style.width = (firstNumber(elem.style.width) - event.movementX) + 'px';
+        });
+
+        resizeRight.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.width = (firstNumber(elem.style.width) + event.movementX) + 'px';
+        });
+        resizeBottom.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.height = (firstNumber(elem.style.height) + event.movementY) + 'px';
+        });
+
+        resizeTopLeft.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.top = (firstNumber(elem.style.top) + event.movementY) + 'px';
+            elem.style.height = (firstNumber(elem.style.height) - event.movementY) + 'px';
+            elem.style.left = (firstNumber(elem.style.left) + event.movementX) + 'px';
+            elem.style.width = (firstNumber(elem.style.width) - event.movementX) + 'px';
+        });
+        resizeTopRight.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.top = (firstNumber(elem.style.top) + event.movementY) + 'px';
+            elem.style.height = (firstNumber(elem.style.height) - event.movementY) + 'px';
+            elem.style.width = (firstNumber(elem.style.width) + event.movementX) + 'px';
+        });
+        resizeBottomLeft.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.height = (firstNumber(elem.style.height) + event.movementY) + 'px';
+            elem.style.left = (firstNumber(elem.style.left) + event.movementX) + 'px';
+            elem.style.width = (firstNumber(elem.style.width) - event.movementX) + 'px';
+        });
+        resizeBottomRight.onmousedown = mouseMove(function(event) {
+            var elem = getWindow();
+            elem.style.width = (firstNumber(elem.style.width) + event.movementX) + 'px';
+            elem.style.height = (firstNumber(elem.style.height) + event.movementY) + 'px';
+        });
+        taskFrameResize.appendChild(resizeLeft);
+        taskFrameResize.appendChild(resizeTop);
+        taskFrameResize.appendChild(resizeBottom);
+        taskFrameResize.appendChild(resizeRight);
+        taskFrameResize.appendChild(resizeTopLeft);
+        taskFrameResize.appendChild(resizeTopRight);
+        taskFrameResize.appendChild(resizeBottomLeft);
+        taskFrameResize.appendChild(resizeBottomRight);
     }
+
+    /* Sets the title for the window, and then appends the title element
+       to the window header, which will later be appended to the window
+       itself.
+    */
     function setTaskFrameTitle() {
-
+        taskFrameWindowTitle.innerText = data.title;
+        taskFrameHeader.appendChild(taskFrameWindowTitle);
     }
+
+    /* Sets the draggable area of the window (the header, including the
+       title, and space around the window buttons.), and then the element
+       is appended to the window header.
+    */
+    function setDragBar() {
+        taskFrameDragbar.onmousedown = function(event) {
+            _taskFrameWindow = document.getElementById(taskFrameWindow.id);
+            WebWindow.Util.CSS.dragdrop(event, 'dragstart', _taskFrameWindow);
+        }
+        taskFrameHeader.appendChild(taskFrameDragbar);
+    }
+
+    /* Temporarily assigns a transition animation for the window, then toggles
+       a class, and then waits the appropriate time to remove the animation from
+       the element.
+    */
+    function maximizeWindow() {
+        var _taskFrameWindow = document.getElementById(taskFrameWindow.id);
+        WebWindow.Util.CSS.addClass(_taskFrameWindow, WebWindow.classes.maximizeAnimation);
+        WebWindow.Util.CSS.toggleClass(_taskFrameWindow, WebWindow.classes.maximize);
+        setTimeout(function() {
+            console.log(_taskFrameWindow);
+            WebWindow.Util.CSS.removeClass(_taskFrameWindow, WebWindow.classes.maximizeAnimation);
+            console.log(_taskFrameWindow);
+        }, 250);
+    }
+
+    /* Temporarily assigns a transition animation for the window, then toggles
+       a class, and then waits the appropriate time to remove the animation
+       from the element.
+    */
+    function minimizeWindow() {
+        var _taskFrameWindow = document.getElementById(taskFrameWindow.id);
+        WebWindow.Util.CSS.addClass(_taskFrameWindow, WebWindow.classes.minimizeAnimation);
+        WebWindow.Util.CSS.toggleClass(_taskFrameWindow, WebWindow.classes.minimize);
+        console.log({ classes :  _taskFrameWindow.className, name : WebWindow.classes.minimize });
+        setTimeout(function() {
+            /* WebWindow.Util.CSS.toggleClass(_taskFrameWindow, WebWindow.classes.invisible); */
+            WebWindow.Util.CSS.removeClass(_taskFrameWindow, WebWindow.classes.minimizeAnimation);
+        }, 350);
+    }
+
+    /* Using a setTimeout() to isolate the removal of the task window from it's
+       parentElement. parentElement is used to ensure that removeChild will work.
+    */
+    function closeWindow() {
+        setTimeout(function() {
+            document.getElementById(taskFrameWindow.id).parentElement.removeChild(taskFrameWindow);
+        });
+    }
+
+    /* Using an initializer array, the items of JSON data are then iterated over,
+       and turned into a task frame button, and then appended to the task window header.
+    */
     function setTaskButtons() {
-
+        var currentTaskButton;
+        [
+            { title : 'O', action : maximizeWindow },
+            { title : '-', action : minimizeWindow },
+            { title : 'X', action : closeWindow }
+        ].forEach(function(taskButton, taskButtonIndex, taskButtonArray) {
+            currentTaskButton = document.createElement('li');
+            currentTaskButton.onclick = taskButton.action;
+            currentTaskButton.innerText = taskButton.title;
+            taskFrameButtons.appendChild(currentTaskButton);
+            currentTaskButton = undefined;
+        });
+        taskFrameHeader.appendChild(taskFrameButtons);
+        taskFrameWindow.appendChild(taskFrameHeader);
     }
-    function getContent() {
 
+    /*
+
+    */
+    function getContent() {
+        return taskFrameContent;
     }
     function setTaskFrameHandlers() {
+        /*
+        task assistant signal listeners.
 
+        taskAssistant.addListeners(taskFrameWindow.id, {
+            'closeall' : function() {},
+            'close' : function() {},
+            'maximize' : function() {},
+            'minimize' : function() {},
+            'resize' : function() {},
+            'move' : function() {}
+        });
+
+        Event handlers for window events will be made soon
+        */
     }
     function toggleVisibility() {
+        var _taskFrameWindow = document.getElementById(taskFrameWindow.id);
+        WebWindow.Util.CSS.toggleFade(_taskFrameWindow);
+    }
+    function create() {
+        /* Appends window resizing tools to the window frame,
+        and over extends the frame boundaries by using CSS */
+        setResizeButtons();
+        /* Sets the title of the window */
+        setTaskFrameTitle();
+        /* Sets the draggable area which is used
+        to move windows in the board */
+        setDragBar();
+        /* Sets the familiar window management buttons on
+        the window in the header */
+        setTaskButtons();
+        /* Adds event handlers to the TaskFrame object, and will
+           respond to Board events such as 'closeall' or 'close'.
+        */
+        setTaskFrameHandlers();
+        taskFrameContent.innerHTML = data.content;
+        taskFrameWindow.appendChild(taskFrameContent);
+        document.getElementById(WebWindow.desktopID)
+            .insertBefore(taskFrameWindow, document.getElementById(WebWindow.beforeElementID));
+        toggleVisibility();
+    }
 
+    /* API created for use of the window. Maximizing, minimizing, moving, closing,
+       content updating, and toggling.
+
+       TODO: add appendChild to updateContent so other elements can be pulled in
+       from the page, and put into a window.
+    */
+    var TaskFrameAPI = {
+        moveTo : function(x, y) {
+            var _taskFrameWindow = document.getElementById(taskFrameWindow.id);
+            _taskFrameWindow.style.left = x + 'px';
+            _taskFrameWindow.style.top = y + 'px';
+        },
+        updateContent : function(updateContent) {
+            taskFrameContent.innerHTML = updateContent;
+        },
+        getWindow : function() {
+            return document.getElementById(taskFrameWindow.id);
+        },
+        toggleWindow : toggleVisibility,
+        minimize : minimizeWindow,
+        maximize : maximizeWindow,
+        close : closeWindow
     }
-    return {
-        getData : function() {},
-        getButtons : function() {},
-        getWindow : function() {},
-        toggleWindow : function() {},
-        getDragBar : function() {},
-        minimize : function() {},
-        maximize : function() {},
-        close : function() {}
-    }
+    /* calls the bootstrap, and then returns the api. */
+    create();
+    return TaskFrameAPI;
 }
 /* Task object for TaskAssistant */
 WebWindow.Task = function(data, taskAssistant) {
